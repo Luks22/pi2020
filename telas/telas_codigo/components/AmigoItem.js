d441355816384b983f989e544ea12f4c399b2e40
Soosbr <matheusosoos@hotmail.com>
2020-04-30 01:56:11 -0300

    Adição da FlatList e ajustes no CSS

--- telas/telas_codigo/components/AmigoItem.js
+++ telas/telas_codigo/components/AmigoItem.js
@@ -8,21 +8,23 @@ const AmigoItem = (props) => {
         <View style={styles.amigoItem} >
             <View style={styles.itemLista}>
                 <View>
-                    <Text>
-                        Nome: {props.nome}
-                    </Text>
-                    <Text>
-                        Telefone : {props.celular}
-                    </Text>
+                    <View style={styles.textField}>
+                        <Text style={styles.text}>Nome: </Text>
+                        <Text>{props.nome}</Text>
+                    </View>
+                    <View style={styles.textField}>
+                        <Text style={styles.text}>Telefone: </Text>
+                        <Text>{props.celular}</Text>
+                    </View>
                 </View>
                 <View style={styles.buttonView}>
-                    <View style={{marginRight: 6}}>
+                    <View style={{ marginRight: 6 }}>
                         <Button title='Localizar' />
                     </View>
                     <View>
-                        <Button 
-                            title='Excluir' 
-                            color= '#fa6464'
+                        <Button
+                            title='Excluir'
+                            color='#fa6464'
                         />
                     </View>
                 </View>
@@ -35,7 +37,6 @@ const AmigoItem = (props) => {
 const styles = StyleSheet.create({
     amigoItem: {
         paddingHorizontal: 6,
-        paddingTop: 12,
     },
     buttonView: {
         flexDirection: 'row',
@@ -43,11 +44,21 @@ const styles = StyleSheet.create({
     },
     itemLista: {
         flexDirection: 'column',
-        paddingLeft: 6,
         borderWidth: 1,
         borderColor: '#00000066',
-        marginTop: 8,
-        marginHorizontal: 6
+        marginVertical: 8,
+        marginHorizontal: 6,
+        paddingTop: 8,
+        paddingHorizontal: 6
+    },
+    textField: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 15,
     }
 })
 
--- telas/telas_codigo/telas/TelaPrincipal.js
+++ telas/telas_codigo/telas/TelaPrincipal.js
@@ -1,45 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, View, StyleSheet, Text, FlatList, ScrollView } from 'react-native'
 import AmigoItem from '../components/AmigoItem'
 
 const TelaPrincipal = (props) => {
+    const [users, setUsers] = useState([
+        {key: 1, nome: "Teste1", usuario: "test1", senha: "2222", celular: "977773333"},
+        {key: 2, nome: "Teste2", usuario: "test2", senha: "2222", celular: "977773333"},
+        {key: 3, nome: "Teste3", usuario: "test3", senha: "2222", celular: "977773333"},
+        {key: 4, nome: "Teste4", usuario: "test4", senha: "2222", celular: "977773333"},
+        {key: 5, nome: "Teste5", usuario: "test5", senha: "2222", celular: "977773333"},
+        {key: 6, nome: "Teste6", usuario: "test6", senha: "2222", celular: "977773333"},
+        {key: 7, nome: "Teste7", usuario: "test7", senha: "2222", celular: "977773333"},
+        {key: 8, nome: "Teste8", usuario: "test8", senha: "2222", celular: "977773333"},
+    ])
+
     return (
-        <ScrollView>
-            <View style={styles.tela}>
-                <View style={styles.leftView}>
-                    <View style={styles.buttonView} >
-                        <View style={{ paddingLeft: 8, marginBottom: 8 }}>
-                            <Button title='Editar Perfil' 
-                                onPress={props.onEditar}
-                            />
-                        </View>
-                        <View style={{ paddingLeft: 8 }}>
-                            <Button title='Sair'
-                                onPress={props.onSair}
-                            />
-                        </View>
-                        <View style={{ paddingLeft: 8, marginTop: 35 }}>
-                            <Button
-                                title='Add Amigos'
-                                onPress={props.onAddAmigo}
-                            />
-                        </View>
+        <View style={styles.tela}>
+            <View style={styles.leftView}>
+                <View style={styles.buttonView} >
+                    <View style={{ paddingLeft: 8, marginBottom: 8 }}>
+                        <Button title='Editar Perfil'
+                            onPress={props.onEditar}
+                        />
+                    </View>
+                    <View style={{ paddingLeft: 8 }}>
+                        <Button title='Sair'
+                            onPress={props.onSair}
+                        />
+                    </View>
+                    <View style={{ paddingLeft: 8, marginTop: 35 }}>
+                        <Button
+                            title='Add Amigos'
+                            onPress={props.onAddAmigo}
+                        />
                     </View>
                 </View>
-                <View style={styles.rightView}>
-                    <AmigoItem />
-                    <AmigoItem />
-                    <AmigoItem />
-                    <AmigoItem />
-                </View>
             </View>
-        </ScrollView>
+            <View style={styles.rightView}>
+                <FlatList
+                    data={users}
+                    renderItem={
+                        user => (
+                            <AmigoItem
+                                chave={user.item.key}
+                                nome={user.item.nome}
+                                celular={user.item.celular}
+                            />
+                        )
+                    }
+                />
+            </View>
+        </View>
     )
 
 }
 const styles = StyleSheet.create({
     tela: {
-        marginTop: 35,
+        marginTop: 15,
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'center',
@@ -51,11 +68,12 @@ const styles = StyleSheet.create({
     },
     rightView: {
         borderWidth: 1,
-        paddingBottom: 20,
+        paddingVertical: 10,
+        paddingHorizontal: 6,
         borderRadius: 30,
-        padding: 6
     },
     leftView: {
+        flex: 1,
         alignItems: 'center',
     }
 
